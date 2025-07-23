dc06da85c9a4830e8041a6f4ac8eb95d7342234d
elisha111 <elisey.kovalyov@yandex.ru>
2025-07-24 00:59:05 +0300

    #9 feat(slider): js-модуль sliderСollection

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,72 +1,25 @@
 import Section from "@/layouts/Section"
 import "./Categories.scss"
 import CategoryCart from "@/components/CategoryCart"
+import Slider from "@/components/Slider"
+import SliderNavigation from "@/components/Slider/components/SliderNavigation"
+import categoryItems from "./categoryItem"
 
 const Categories = () => {
-  const categoryItems = [
-    {
-      title: "Actions",
-      images: [
-        "/src/assets/images/categories/action/1.jpg",
-        "/src/assets/images/categories/action/2.jpg",
-        "/src/assets/images/categories/action/3.jpg",
-        "/src/assets/images/categories/action/4.jpg",
-      ],
-    },
-    {
-      title: "Adventure",
-      images: [
-        "/src/assets/images/categories/action/1.jpg",
-        "/src/assets/images/categories/action/2.jpg",
-        "/src/assets/images/categories/action/3.jpg",
-        "/src/assets/images/categories/action/4.jpg",
-      ],
-    },
-    {
-      title: "Comedy",
-      images: [
-        "/src/assets/images/categories/action/1.jpg",
-        "/src/assets/images/categories/action/2.jpg",
-        "/src/assets/images/categories/action/3.jpg",
-        "/src/assets/images/categories/action/4.jpg",
-      ],
-    },
-    {
-      title: "Drama",
-      images: [
-        "/src/assets/images/categories/action/1.jpg",
-        "/src/assets/images/categories/action/2.jpg",
-        "/src/assets/images/categories/action/3.jpg",
-        "/src/assets/images/categories/action/4.jpg",
-      ],
-    },
-    {
-      title: "Horror",
-      images: [
-        "/src/assets/images/categories/action/1.jpg",
-        "/src/assets/images/categories/action/2.jpg",
-        "/src/assets/images/categories/action/3.jpg",
-        "/src/assets/images/categories/action/4.jpg",
-      ],
-    },
-  ]
-
+  const sliderNavigationId = "categories-slider-navigation"
   return (
     <Section
       title="Explore our wide variety of categories"
       titleId="categories-title"
       description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
-      actions={
-        <div>
-          <button>dgthtl</button>
-          <button>назад</button>
-        </div>
-      }
+      actions={<SliderNavigation mode="tile" id={sliderNavigationId} />}
       isActionsHiddenOnMobile
     >
-      {categoryItems.map((categoryItem, index) => (
-        <CategoryCart {...categoryItem} key={index} />
-      ))}
+      <Slider navigationTargetElementId={sliderNavigationId}>
+        {categoryItems.map((categoryItem, index) => (
+          <CategoryCart {...categoryItem} key={index} />
+        ))}
+      </Slider>
     </Section>
   )
 }
