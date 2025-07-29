0c827e5e6220ecc1609ec50a81678d5e1f7f0bb5
kovalev.e <kovalev.e@web-canape.ru>
2025-07-29 10:22:41 +0300

    #14 feat(collection): новая секция коллекций + добавление слайдера

---
+++ src/sections/Collections/collectionGroups.js
@@ -0,0 +1,214 @@
+import categoryItems from "@/sections/Categories/categoryItem"
+
+const collectionGroups = [
+  {
+    isActive: true,
+    title: "Movies",
+    items: [
+      {
+        title: "Our Genres",
+        categoryItems,
+      },
+      {
+        title: "Popular Top 10 In Genres",
+        categoryItems: [
+          {
+            title: "Action 1",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Adventure 1",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Comedy 1",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Drama 1",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+
+          {
+            title: "Action 2",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Adventure 2",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Comedy 2",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Drama 2",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+
+          {
+            title: "Action 3",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Adventure 3",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Comedy 3",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Drama 3",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+
+          {
+            title: "Action 4",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Adventure 4",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Comedy 4",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+          {
+            title: "Drama 4",
+            badge: "Top 10 In",
+            images: [
+              "/src/assets/images/categories/action/1.jpg",
+              "/src/assets/images/categories/action/2.jpg",
+              "/src/assets/images/categories/action/3.jpg",
+              "/src/assets/images/categories/action/4.jpg",
+            ],
+          },
+        ],
+        sliderParams: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 30,
+          breakpoints: {
+            0: {
+              slidesPerView: 1.6,
+              slidesPerGroup: 1,
+              spaceBetween: 20,
+            },
+            481: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+              spaceBetween: 20,
+            },
+            1024: {
+              spaceBetween: 20,
+              allowTouchMove: false,
+            },
+            1441: {
+              spaceBetween: 30,
+              allowTouchMove: false,
+            },
+          },
+        },
+      },
+    ],
+  },
+]
+
+export default collectionGroups
