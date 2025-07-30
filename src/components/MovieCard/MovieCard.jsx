f7363233e65500bf43e0abeeb5f8e1d410058735
kovalev.e <kovalev.e@web-canape.ru>
2025-07-30 15:07:44 +0300

    #15 feat(movieCard): компонент карточки MovieCard

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,29 @@
+import { Image } from "minista"
+import "./MovieCard.scss"
+import Badge from "../Badge"
+
+const MovieCard = (params) => {
+  const { title, imgSrc, duration, views, href = "/movie" } = params
+
+  return (
+    <a className="movie-card" href={href} title={title}>
+      <h3 className="visually-hidden">{title}</h3>
+      <Image className="movie-card__image" src={imgSrc} />
+      <div className="movie-card__body">
+        {duration && (
+          <Badge iconName="clock" iconAriaLabel="Duration" hasFillIcon>
+            {duration}
+          </Badge>
+        )}
+
+        {views && (
+          <Badge iconName="eye" iconAriaLabel="Views" hasFillIcon>
+            {views}
+          </Badge>
+        )}
+      </div>
+    </a>
+  )
+}
+
+export default MovieCard
