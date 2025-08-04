fcaff91ac99512e6fd485d737568bce5b1056805
kovalev.e <kovalev.e@web-canape.ru>
2025-08-04 12:30:04 +0300

    #16 feat(personCard): новый компонент карточки актеров

---
+++ src/components/PersonCard/PersonCard.jsx
@@ -0,0 +1,19 @@
+import { Image } from "minista"
+import "./PersonCard.scss"
+
+const PersonCard = (props) => {
+  const { imgSrc, imgAlt } = props
+
+  return (
+    <div className="person-card">
+      <Image
+        className="person-card__image"
+        src={imgSrc}
+        alt={imgAlt}
+        title={imgAlt}
+      />
+    </div>
+  )
+}
+
+export default PersonCard
