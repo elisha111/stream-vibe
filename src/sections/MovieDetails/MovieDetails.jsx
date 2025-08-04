cab0d30bf12a87173f5ac55296f4b87a37960d43
kovalev.e <kovalev.e@web-canape.ru>
2025-08-04 12:35:16 +0300

    #16 refactor(movieDetails): добавление масива элементов для castItems и rewiewItems

--- src/sections/MovieDetails/MovieDetails.jsx
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,89 @@
 import SliderNavigation from "@/components/Slider/components/SliderNavigation"
 import "./MovieDetails.scss"
 import Slider from "@/components/Slider"
+import PersonCard from "@/components/PersonCard"
+import Button from "@/components/Button"
+import ReviewCard from "@/components/ReviewCard"
 
 const MovieDetails = () => {
   const titleId = "movie-details-title"
   const castSliderNavigationId = "movie-card-slider-navigation"
+
+  const castItems = [
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+    { imgSrc: "/src/assets/images/people/cast.jpg", imgAlt: "Ivan Ivanov" },
+  ]
+
+  const reviewItems = [
+    {
+      name: "Aniket Roy",
+      subtitle: "From India",
+      description:
+        "This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.",
+      ratingValue: 4.5,
+    },
+    {
+      name: "Swaraj",
+      subtitle: "From India",
+      description:
+        "A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.",
+      ratingValue: 5,
+    },
+    {
+      name: "Aniket Roy 2",
+      subtitle: "From India",
+      description:
+        "This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.",
+      ratingValue: 4.5,
+    },
+    {
+      name: "Swaraj 2",
+      subtitle: "From India",
+      description:
+        "A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.",
+      ratingValue: 5,
+    },
+    {
+      name: "Aniket Roy",
+      subtitle: "From India",
+      description:
+        "This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.",
+      ratingValue: 4.5,
+    },
+    {
+      name: "Swaraj",
+      subtitle: "From India",
+      description:
+        "A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.",
+      ratingValue: 5,
+    },
+    {
+      name: "Aniket Roy 2",
+      subtitle: "From India",
+      description:
+        "This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.",
+      ratingValue: 4.5,
+    },
+    {
+      name: "Swaraj 2",
+      subtitle: "From India",
+      description:
+        "A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.",
+      ratingValue: 5,
+    },
+  ]
 
   return (
     <section className="movie-details container" aria-labelledby={titleId}>
@@ -25,7 +104,7 @@ const MovieDetails = () => {
           </div>
         </div>
         <div className="movie-datails__panel">
-          <header className="movie-details__header">
+          <header className="movie-details__panel-header">
             <h3 className="movie-details__title">Cast</h3>
             <SliderNavigation
               id={castSliderNavigationId}
@@ -37,9 +116,8 @@ const MovieDetails = () => {
           <Slider
             navigationTargetElementId={castSliderNavigationId}
             hasScrollbarOnMobile={false}
-            slideParams={{
+            sliderParams={{
               slidesPerView: "auto",
-              slidesPerGroup: 5,
               spaceBetween: 10,
               breakpoints: {
                 1024: {
@@ -49,10 +127,48 @@ const MovieDetails = () => {
               },
             }}
           >
-            
+            {castItems.map((castItem, index) => (
+              <PersonCard {...castItem} key={index} />
+            ))}
           </Slider>
         </div>
-        <div className="movie-datails__panel"></div>
+        <div className="movie-datails__panel">
+          <header className="movie-details__panel-header">
+            <h3 className="movie-details__title">Reviews</h3>
+            <Button
+              label="Add Your Review"
+              mode="black-08"
+              iconName="plus"
+              href="/"
+            />
+          </header>
+          <Slider
+            navigationMode="rounded"
+            isNavigationHiddenMobile={false}
+            hasScrollbarOnMobile={false}
+            sliderParams={{
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              breakpoints: {
+                0: {
+                  slidesPerView: 1,
+                  slidesPerGroup: 1,
+                  spaceBetween: 16,
+                },
+                1024: {
+                  slidesPerView: 2,
+                  slidesPerGroup: 2,
+                  spaceBetween: 20,
+                  allowTouchMove: false,
+                },
+              },
+            }}
+          >
+            {reviewItems.map((reviewItem, index) => (
+              <ReviewCard {...reviewItem} key={index} />
+            ))}
+          </Slider>
+        </div>
       </div>
       <aside className="movie-details__info">
         <div className="movie-datails__panel"></div>
