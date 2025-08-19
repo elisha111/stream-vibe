b11a8f9af6c66834201fa98558f2fbd76cd74099
kovalev.e <kovalev.e@web-canape.ru>
2025-08-19 10:23:32 +0300

    #6.3 feat(showCard): компонет карточки сериала

---
+++ src/components/EpisodeCard/EpisodeCard.jsx
@@ -0,0 +1,44 @@
+import Icon from "@/components/Icon"
+import "./EpisodeCard.scss"
+
+const EpisodeCard = (props) => {
+  const { number, title, description, duration, video } = props
+
+  const playButtonTitle = "Play video"
+
+  return (
+    <div className="episode-card">
+      <div className="episode-card__number">{number}</div>
+      <div className="episode-card__player">
+        <video
+          className="episode-card__video"
+          src={video.src}
+          poster={video.poster}
+          width={172}
+          height={118}
+        />
+        <button
+          className="episode-card__play-button is-active"
+          aria-label={playButtonTitle}
+          title={playButtonTitle}
+        >
+          <Icon className="episode-card__play-button-icon" name="play-circle" />
+        </button>
+      </div>
+      <div className="episode-card__body">
+        <div className="episode-card__info">
+          <h4 className="episode-card__title h6">{title}</h4>
+          <div className="episode-card__duration">
+            <Icon name="clock_stroke" aria-label="duration" />
+            <span>{duration}</span>
+          </div>
+        </div>
+        <div className="episode-card__description hidden-mobile">
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EpisodeCard
