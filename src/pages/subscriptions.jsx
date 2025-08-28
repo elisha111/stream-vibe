fbee852d1a7b98df0c7c2a08d8af73754ad4f65b
kovalev.e <kovalev.e@web-canape.ru>
2025-08-28 17:33:55 +0300

    #8.1 feat(plansComparison): блок сравнения(только таблица)

---
+++ src/pages/subscriptions.jsx
@@ -0,0 +1,15 @@
+import PlansComparison from "@/components/PlansComparison"
+import Plans from "@/sections/Plans"
+
+export const metadata = {
+  title: "Subscriptions",
+}
+
+export default function () {
+  return (
+    <>
+      <Plans />
+      <PlansComparison />
+    </>
+  )
+}
